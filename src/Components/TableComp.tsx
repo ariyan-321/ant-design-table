65ab728a35cc1ca83f024e92e4c07d1b9e959bb6
Your Name <ariyaniphone14@gmail.com>
2025-03-05 10:18:08 +0600

    added comments to make code more readable

--- src/Components/TableComp.tsx
+++ src/Components/TableComp.tsx
@@ -15,25 +15,32 @@ interface ColumnType {
 }
 
 export default function TableComp() {
+
+    // columns state
   const [columns, setColumns] = useState<ColumnType[]>([
     { title: "Id", dataIndex: "id" },
     { title: "Title", dataIndex: "title" },
     { title: "Price", dataIndex: "price" },
   ]);
 
+//   loading state
   const [loading, setLoading] = useState<boolean>(true);
 
+//   for storing data
   const [dataSource, setDataSource] = useState<Product[]>([]);
 
 
  
 
+//   storing visible columns in localStorage
   const [visibleColumns, setVisibleColumns] = useState<string[]>(
     () =>
       JSON.parse(localStorage.getItem("visibleColumns") || "[]") ||
       columns.map((col) => col.dataIndex)
   );
 
+
+//   data fetching
   useEffect(() => {
     setLoading(true);
     axios
@@ -42,6 +49,8 @@ export default function TableComp() {
       setLoading(false);
   }, []);
 
+
+//   toggle visibility
   const handleToggle = (columnKey: string) => {
     setVisibleColumns((prev) => {
       const newVisibleColumns = prev.includes(columnKey)
@@ -53,9 +62,14 @@ export default function TableComp() {
     });
   };
 
+//   filter columns
   const filteredColumns = columns.filter((col) =>
     visibleColumns.includes(col.dataIndex)
   );
+
+
+
+//   column menu
 
   const columnMenu = (
     <Menu>
@@ -79,12 +93,14 @@ export default function TableComp() {
       </h1>
       <div className="border-4 rounded-xl p-12 shadow-xl">
         <div className="flex justify-end p-5">
+            {/* dropdown for toggling */}
           <Dropdown overlay={columnMenu} trigger={["click"]}>
             <Button>
               Columns <DownOutlined />
             </Button>
           </Dropdown>
         </div>
+        {/* table */}
         <Table
           columns={filteredColumns}
           loading={loading}
